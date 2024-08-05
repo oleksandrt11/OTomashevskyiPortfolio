be219cad4cbf8b5d758c4781dd42e32d5663334f
oleksandrt11 <oleksandrtomasevskij04@gmail.com>
2024-08-05 16:53:04 +0000

    Added a new button

--- script.js
+++ script.js
@@ -16,12 +16,29 @@
 
 
 
-var age= prompt('How old are you?');
 
-if( age < 18){
-  alert('Minor ');
-} else if(age >= 18 && typeof age==="string" <= 55){
-  alert('Adult');
-} else {
-  alert('Senior'); 
+
+
+
+function AgeCheck (age){
+  var age= prompt('How old are you?');
+
+  if( age < 18){
+    alert('Minor ');
+  } else if(age >= 18 && age <= 55){
+    alert('Adult');
+  } else {
+    alert('Senior'); 
+  }
 }
+ // AgeCheck()
+
+
+// function introduction(age,name,location){
+//   console.log(age)
+//   console.log(name)
+//   console.log(location)
+//   console.log("My name is " + name + " and I am " + age + " years old. I live in " +location)
+
+// }
+// introduction(18, "Oleksandr","NYC")
