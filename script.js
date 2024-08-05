d276723287fc783566d2689a0048b3f02c188f8e
oleksandrt11 <oleksandrtomasevskij04@gmail.com>
2024-08-05 13:29:27 +0000

    Added JavaScript and made a small app

---
+++ script.js
@@ -0,0 +1,27 @@
+// this is a comment
+
+// console.log("Hello World");
+
+
+// var prompt = prompt("What is your name?");
+// console.log(prompt)
+
+
+//  if(10>9){
+//   console.log('true');
+// }else{
+//  console.log('this is false');
+// }
+
+
+
+
+var age= prompt('How old are you?');
+
+if( age < 18){
+  alert('Minor ');
+} else if(age >= 18 && typeof age==="string" <= 55){
+  alert('Adult');
+} else {
+  alert('Senior'); 
+}
