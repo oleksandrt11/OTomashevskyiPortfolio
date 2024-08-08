d23b287f4b6f360e7d49fe09375188882bae32f5
oleksandrt11 <oleksandrtomasevskij04@gmail.com>
2024-08-08 16:50:05 +0000

    Did the functionality of plant id and some css

--- script.js
+++ script.js
@@ -42,3 +42,58 @@ function AgeCheck (age){
 
 // }
 // introduction(18, "Oleksandr","NYC")
+
+
+
+
+
+function uploadAndIdentifyPlantID(){
+  var  photoInput = document.getElementById("photoInput");
+ console.log(photoInput)
+  console.log(photoInput.files[0])
+
+  
+  if (photoInput.files.length===0){
+    alert("Please select a photo to upload");
+    return
+  }
+  // catch and store the file
+  var selectedfile = photoInput.files[0];
+  
+  var reader = new FileReader();
+
+  // event handler and doing API's
+  reader.onload = function(e){
+    var base64Image = e.target.result;
+
+    var apiKey = "gaJUdRaqQVclr3ukFg1EMXtvkfcEwwxzSKn48kJLjSRDbngIdL" ;
+    var latitude = 49.207;
+    var longitude = 16.608 ;
+    var health = "all";
+    var similarimages = true;
+    var details = "common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods,treatment,cause"
+    var language = "en";
+    var apiURL = `https://plant.id/api/v3/identification?details={details}&language=${language}`;
+  }
+
+axios.post(
+  apiURL,
+  {
+    images: [base64Image],
+    latitude: latitude,
+    longitude: longitude,
+    health: health,
+    similarimages: similarimages
+  },
+  {
+    headers: {
+      "Api-key": apiKey,
+      "Content-Type": "application/json"
+       }
+     }
+  )
+)
+  
+}
+
+ 
